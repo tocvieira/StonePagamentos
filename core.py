14af8261b657b1bc7f59cb9e949f409dec486b4c
Thiago Vieira <thiago@thiagovieira.adv.br>
2017-10-27 10:47:47 -0300

    first commit

---
+++ core.py
@@ -0,0 +1,86 @@
+from xml.etree.ElementTree import fromstring
+from collections import defaultdict
+import builtins
+import datetime
+import requests
+import configparser
+from datetime import date, timedelta
+
+def get_autorization():
+    """ Get Stone Token"""
+    config = configparser.ConfigParser()
+    config.read('config.txt', encoding="utf-8")
+    autorization = config.get('configuration', 'authorization')
+    return autorization
+
+def get_stone_report():
+    """ Get Stone XML """
+
+    today = datetime.datetime.strftime(datetime.datetime.now(), '%Y%m%d')
+    yesterday = date.today() - timedelta(1)
+    url = 'https://conciliation.stone.com.br/conciliation-file/v2/{}'.format(yesterday.strftime('%Y%m%d'))
+    headers = {
+        "Authorization":get_autorization(),
+        # "Accept-Encoding": "gzip"
+    }
+    r = requests.get(url, headers=headers)
+    return r.text
+
+def get_gross_amount(tree):
+    """ Parse Gross Amount """
+    r = []
+    for item in tree.findall("FinancialTransactions"):
+        for x in item.findall('Transaction'):
+            for y in x.findall('Installments'):
+                for k in y.findall('Installment'):
+                    bruto = k.find('GrossAmount')
+                    r.append(float(bruto.text))
+    return (sum(r))
+
+def get_net_amount(tree):
+    """ Parse Net Amount """
+    r = []
+    for item in tree.findall("FinancialTransactions"):
+        for x in item.findall('Transaction'):
+            for y in x.findall('Installments'):
+                for k in y.findall('Installment'):
+                    bruto = k.find('NetAmount')
+                    r.append(float(bruto.text))
+    return (sum(r))
+
+def get_prevision(tree):
+    """ Paser Prevision Dates """
+    l_net_amount = []
+    l_prevision_date = []
+    for item in tree.findall("FinancialTransactions"):
+        for x in item.findall('Transaction'):
+            for y in x.findall('Installments'):
+                for k in y.findall('Installment'):
+                    v_net_amount = k.find('NetAmount')
+                    v_prevision_date = k.find('PrevisionPaymentDate')
+                    l_net_amount.append(float(v_net_amount.text))
+                    l_prevision_date.append(int(v_prevision_date.text))
+    r = list(zip(l_prevision_date, l_net_amount))
+    d = defaultdict(list)
+    for k, v in r:
+        d[k].append(v)
+    return (d)
+
+def main():
+    """ Main Functional"""
+    tree = fromstring(get_stone_report())
+
+    gross_amount = get_gross_amount(tree)
+    net_amount = get_net_amount(tree)
+    tax = gross_amount - net_amount
+    prevision = builtins.dict(get_prevision(tree))
+
+    print("Valor bruto das transações: R$ {:.02f}".format(gross_amount))
+    print("Valor líquído das transações: R$ {:.02f}".format(net_amount))
+    print("Total descontado: R$ {:.02f}".format(tax))
+    print("Os valores líquidos serão repassados com a seguinte previsão:")
+    for x, k in prevision.items():
+        print("Data: {} Valor: R$ {:.02f}".format(x, sum(k)))
+
+if __name__ == '__main__':
+    main()
