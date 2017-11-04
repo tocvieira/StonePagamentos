58387d7d738ca48a909ca06490f8861ae8590504
Thiago Vieira <thiago@localhost.localdomain>
2017-11-04 06:43:58 -0300

    Big update: now software picks up reports from a date range and stores them in a SQLite3 database

--- core.py
+++ core.py
@@ -5,9 +5,9 @@ import sqlite3
 import requests
 import configparser
 import os
+from datetime import date, timedelta, datetime
 
-# import datetime
-#from datetime import date, timedelta
+
 
 def get_autorization():
     """ Get Stone Token"""
@@ -92,7 +92,7 @@ def table_create():
     );
     """)
 
-    print("Tabela Criada com Sucesso")
+    print("ATENÇÃO - Um novo banco de dados foi criado")
 
     database.close()
 
@@ -100,31 +100,57 @@ def insert_data(report_date, date, value):
     """ Insert data in DataStone.db """
     conn = sqlite3.connect('DataStone.db')
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO DataStone (report_date, date, value) VALUES (?, ?, ?)", (report_date, date, value))
+    cursor.execute("INSERT INTO DataStone (report_date, date, value) VALUES (?, ?, ?)",
+                   (report_date, date, value))
     conn.commit()
     conn.close()
+
+def get_archive_report_date():
+    conn = sqlite3.connect('DataStone.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM DataStone')
+
+    rows = cursor.fetchall()
+    archive_report = ([row[1] for row in rows])
+    conn.close()
+
+    return archive_report
 
 def main():
     """ Main Functional"""
 
     table_create()
 
-    report_date = input("Informe a data desejada (Exemplo: 20171026): ")
-    #report_date = "20171026" #date.today() - timedelta(1)
-    tree = fromstring(get_stone_report(report_date))
+    start_date = datetime.strptime(input("Informe a data inicial (Exemplo: 20171001): "), '%Y%m%d')
+    end_date = datetime.strptime(input("Informe a data final: "), '%Y%m%d')
 
-    gross_amount = get_gross_amount(tree)
-    net_amount = get_net_amount(tree)
-    tax = gross_amount - net_amount
-    prevision = builtins.dict(get_prevision(tree))
+    delta = end_date - start_date
+    print(range(delta.days +1))
 
-    print("Valor bruto das transações: R$ {:.02f}".format(gross_amount))
-    print("Valor líquído das transações: R$ {:.02f}".format(net_amount))
-    print("Total descontado: R$ {:.02f}".format(tax))
-    print("Os valores líquidos serão repassados com a seguinte previsão:")
-    for x, k in prevision.items():
-        print("Data: {} Valor: R$ {:.02f}".format(x, sum(k)))
-        insert_data(report_date, x, sum(k))
+    for y in range(delta.days + 1):
+        report_date = start_date + timedelta(days=y)
+        report_date = report_date.strftime('%Y%m%d')
+        print("\n\nObtendo o relatório do dia {}/{}/{}".format(report_date[6:8], report_date[4:6], report_date[0:4]))
+        #report_date = "20171026" #date.today() - timedelta(1)
+        tree = fromstring(get_stone_report(report_date))
+
+        gross_amount = get_gross_amount(tree)
+        net_amount = get_net_amount(tree)
+        tax = gross_amount - net_amount
+        prevision = builtins.dict(get_prevision(tree))
+
+        print("Valor bruto das transações: R$ {:.02f}".format(gross_amount))
+        print("Valor líquído das transações: R$ {:.02f}".format(net_amount))
+        print("Total descontado: R$ {:.02f}".format(tax))
+        print("Os valores líquidos serão repassados com a seguinte previsão:")
+        archive_report = get_archive_report_date()
+        if int(report_date) in archive_report:
+            print("WARNING: O relatório do dia {} já foi importado.".format(report_date))
+        else:
+            for x, k in prevision.items():
+                print("Data: {} Valor: R$ {:.02f}".format(x, sum(k)))
+                insert_data(report_date, x, sum(k))
+            print("Dados arquivados com sucesso")
 
 
 if __name__ == '__main__':
